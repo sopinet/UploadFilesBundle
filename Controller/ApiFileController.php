d238d1daf2606beb62cc31c5748425cb55442648
alejandro <aarrabal@sopinet.com>
2018-02-02 13:08:36 +0100

    Added cropper option

--- Controller/ApiFileController.php
+++ Controller/ApiFileController.php
@@ -14,12 +14,15 @@ namespace Sopinet\UploadFilesBundle\Controller;
 use Doctrine\ORM\EntityManager;
 use FOS\RestBundle\Controller\FOSRestController;
 use FOS\RestBundle\Controller\Annotations\Post;
+use FOS\RestBundle\Controller\Annotations\Get;
 use AppBundle\Entity\Oil;
 
 use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Symfony\Component\HttpFoundation\File\File;
 use Symfony\Component\HttpFoundation\Request;
 use Nelmio\ApiDocBundle\Annotation\ApiDoc;
+use Symfony\Component\HttpFoundation\Response;
 
 class ApiFileController extends FOSRestController {
 
@@ -53,5 +56,30 @@ class ApiFileController extends FOSRestController {
         }
 
         return $this->get('sopinet_apihelperbundle_apihelper')->msgDenied();
+    }
+
+    /**
+     * @ApiDoc(
+     *   description="Devuelve un fichero de la session"
+     * )
+     *
+     * Borra un fichero del directorio orphanmanager del usuario
+     *
+     * @param Request $request
+     * @return mixed
+     *
+     * @GET("/obtainFile/{filename}")
+     */
+    public function obtainFileAction(Request $request, $filename)
+    {
+        $files = $this->get('oneup_uploader.orphanage.gallery')->getFiles();
+        /** @var File $file */
+        foreach ($files as $file) {
+            if ($file->getFileName() == $filename) {
+                $response = new BinaryFileResponse($file);
+                return $response;
+            }
+        }
+        return new Response("", 404);
     }
 } 
